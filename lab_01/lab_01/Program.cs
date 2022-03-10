e47ea2a14fa2737e3d66806982796067ee86fd1e
PWL005 <100776185+PWL005@users.noreply.github.com>
2022-03-10 11:36:01 +0100

    Add files via upload

---
+++ lab_01/lab_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    class ulamek
+    {
+
+        private int licznik, mianownik;
+
+        public ulamek()
+        {
+
+        }
+        public ulamek(string n, int a)
+        {
+
+        }
+        public ulamek(ulamek skroc)
+        {
+
+        }
+
+
+        public interface IEquatable<T>{}
+        public interface IComparable<T> {}
+    }
+}
